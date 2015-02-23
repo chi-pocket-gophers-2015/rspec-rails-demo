9a22bcced98034b2c4b66aae3e1ea44fe72bd11b
Brian Sheridan & Matthew Gray <pair+bsheridan12.mtthwgry@devbootcamp.com>
2015-02-23 15:32:34 -0600

    add controller methods and viewers for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,43 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate, except: [:index, :show]
+
   def index
     @category = Category.find(params[:category_id])
     @articles = @category.articles
   end
+
+  def new
+    @categories = Category.all
+    @article = Article.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.build(article_params)
+    @article.author = current_user
+
+    if @article.save
+      redirect_to category_article_path(@category, @article)
+    else
+      @errors = @article.errors
+      render :new
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, :price)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
