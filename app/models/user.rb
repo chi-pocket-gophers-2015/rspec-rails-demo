1a08b192119b6f840c2ffdcc52d55e88188b0a6e
Brian Sheridan & Matthew Gray <pair+bsheridan12.mtthwgry@devbootcamp.com>
2015-02-23 14:25:15 -0600

    move has_secure_password to the top

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :articles, foreign_key: :author_id
 
   validates :email, :username, presence: true, uniqueness: true
-
-  has_secure_password
-
 end
