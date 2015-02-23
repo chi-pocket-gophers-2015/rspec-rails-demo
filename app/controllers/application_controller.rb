d454f0e38c8610cba01d95807239964ae8545f58
Brian Sheridan & Matthew Gray <pair+bsheridan12.mtthwgry@devbootcamp.com>
2015-02-23 13:49:24 -0600

    add helpers for current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
   protect_from_forgery with: :exception
 end
