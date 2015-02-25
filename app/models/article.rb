7967ef8b4c71c5076e18f91b1d5f3bf96cb19e94
Lucas Willett <me@ltw.io>
2015-02-24 23:13:15 -0600

    no description lol

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
   belongs_to :category
   belongs_to :author, class_name: "User"
 
-  validates  :description, :author, :category, :title, presence: true
+  validates  :author, :category, :title, presence: true
 end
